20fd5aa6718275872657e5534e0079420253c30e
zhiruisong02 <zjs5301@psu.edu>
2020-09-18 12:35:10 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int digit_sum(int n)
+{
+	if(n<10)
+	{
+		return n;
+	}
+	else
+	{
+		int r=n/10;
+		int m=n%10;
+		int s=m+digit_sum(r);
+		return s;
+	}
+}
+
+int main (void){
+  int result = atoi(readline("Enter an int: "));
+  printf("sum of digits is %d\n.", digit_sum(result));
+}
